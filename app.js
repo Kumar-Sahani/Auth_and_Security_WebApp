823d5ea1a94d5773fc0731a2eee7869c085beab8
kumar <klk3k749@gmail.com>
2023-08-14 15:28:05 +0530

    aadded level 2 security - encryted password with a secret key

--- app.js
+++ app.js
@@ -3,18 +3,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
 
+
+//The key and plugin should be before the mongoose model
+//secret key
+const secret = "thisisourlittlesecret";
+//plugin
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+
+//mongoose model
 const User = new mongoose.model("User", userSchema);
+
+
 
 
 app.use(express.static('public'));
