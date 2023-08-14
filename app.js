5de5d349c92bdc368232dd1118030abeab21d0e4
kumar <klk3k749@gmail.com>
2023-08-14 16:18:16 +0530

    added level 3 security - hashed the password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -16,9 +16,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-
-//The plugin should be before the mongoose model
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 //mongoose model
@@ -51,7 +48,7 @@ app.post("/register", async function(req, res){
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     await newUser.save().then(saveDoc => {
@@ -65,7 +62,7 @@ app.post("/register", async function(req, res){
 app.post("/login", async function(req, res){
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
 try{
     await User.findOne({
